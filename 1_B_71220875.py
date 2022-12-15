067ab678d4401edca802954c69b1ae3d8c105932
Hanlig <117884462+Hanlig@users.noreply.github.com>
2022-12-15 13:00:26 +0700

    Add files via upload

---
+++ 1_B_71220875.py
@@ -0,0 +1,21 @@
+import turtle as t
+t.speed()
+t.penup()
+t.pendown()
+
+#creating I
+t.right(90)
+
+t.pensize(40)
+t.color("black")
+t.forward(100)
+t.backward(200)
+t.forward(100)
+t.right(90)
+t.forward(150)
+t.left(90)
+t.forward(100)
+t.backward(200)
+
+
+t.exitonclick()
